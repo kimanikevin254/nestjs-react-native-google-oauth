1f3b3d8e01f652d76430b54e985b14f4fcf3b6af
Kevin Kim <kimanikevin254@gmail.com>
2024-08-04 19:33:23 +0300

    mobile app structure

---
+++ mobileApp/app/_layout.tsx
@@ -0,0 +1,38 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { useFonts } from 'expo-font';
+import { Slot, Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    ...FontAwesome.font,
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
+
+  return (
+    <SafeAreaProvider>
+      <Slot initialRouteName='/(tabs)' />
+    </SafeAreaProvider>
+  );
+}
